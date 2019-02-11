88e26845d0203de8b88feee3c23e9bd127c8234d
Rakesh Mukundan <rakesh@nibblecomm.com>
2019-02-11 11:14:44 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='emma',
+    version='0.',
+    packages=['emma',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=['requests==2.21.0'], 
+)
